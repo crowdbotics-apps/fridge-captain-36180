8e1788c97bfe624313da2ad8785c1f2241decf44
Crowdbotics <team@crowdbotics.com>
2022-07-13 20:58:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "fridge_captain_36180",
+  url: "https://fridge_captain_36180.botics.co",
+  api: "https://fridge_captain_36180.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
